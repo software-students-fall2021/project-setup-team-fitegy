09e6b3ceee29de394dfcbdc93548ca6f3b37cb81
Corey Dubin <dubincorey@gmail.com>
2021-10-24 22:39:49 -0400

    removed googleapi for MaterialIcon

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,10 @@
-import ChallengeManager from "./components/challengeManager/challengeManager";
+import ChallengeManager from "./components/ChallengeManager/ChallengeManager";
 
 function App() {
   return (
-    <div>
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      />
-      <meta name="viewport" content="initial-scale=1, width=device-width" />
+    <>
       <ChallengeManager />
-    </div>
+    </>
   );
 }
 
