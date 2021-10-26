1861092b9e67c8762943b2eb839f978623f9a0fd
Salma-Hashem <sh5640@nyu.edu>
2021-10-26 17:24:51 -0400

    files for "Create a new..." Screen to select Type

---
+++ front-end/src/Components/CreateChallenge/createChallenge.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+import "./createChallenge.css"
+import IconButton from '@mui/material/IconButton';
+
+
+const Details = (props) =>{
+    return(
+        <>
+        </>
+    )
+}
+const CreateChallenge = () =>{
+
+    return(
+        <div id="Challenge" style={{ backgroundImage: "url('/images/background.png')" }}>
+
+        </div>
+    )
+}
