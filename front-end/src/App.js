fb640fe890f6c0f1f7207ce884f836ba9c9b97a9
Alex Ko <fyk211@nyu.edu>
2021-10-26 18:38:23 -0400

    upload notification page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Feed from "./components/feed/feed"
+import Notification_page from "./components/notification_page/notification_page";
 
 function App() {
 
   return (
     <div id="screen">
       <Feed />
+      <Notification_page /> 
     </div>
   );
 }
