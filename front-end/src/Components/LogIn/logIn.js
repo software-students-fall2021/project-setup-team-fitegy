658410a61924f8f74ed7418f4a6b2547b7a6fb21
Salma-Hashem <sh5640@nyu.edu>
2021-10-26 23:44:16 -0400

    LogIn screen

---
+++ front-end/src/Components/LogIn/logIn.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import "./LogIn.css"
+
+
+const LogIn = () =>{
+    const [name, setName] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    const handleSubmit = (event) => {
+        console.log(`
+        Username: ${name}
+        Password: ${password}
+       
+
+        `);
+    
+    event.preventDefault();
+    }
+
+  return (
+    <div id="Challenge" style={{ backgroundImage: "url('/images/background.png')" }}>
+        <form onSubmit={handleSubmit}>
+            <h1></h1>
+           
+            <label>
+                Username:
+                <input
+                name="name"
+                type="name"
+                value={name}
+                onChange={e => setName(e.target.value)}
+                required />
+            </label>
+            
+            <label>
+                Password:
+                <input
+                name="password"
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                required />
+            </label>
+            <button >Log In</button>
+
+        
+
+
+        </form>
+  
+    </div>
+    );
+}
+export default LogIn;
