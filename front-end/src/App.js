191d3557e43583b97346a6649639fb59ab66ce2f
Alex Ko <fyk211@nyu.edu>
2021-10-27 14:55:17 -0400

    fix display

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,25 +3,23 @@ import CreateNew from "./Components/CreateNew/createNew.js";
 import ProfileBar from "./Components/ProfileBar/ProfileBar.js";
 import React, { useState, useEffect } from "react";
 import Feed from "./Components/feed/feed";
-import NotificationPage from "./Components/notification_page/notification_page";
 import CreateChallenge from "./Components/CreateChallenge/createChallenge.js";
 import ChallengeManager from "./Components/ChallengeManager/ChallengeManager";
 import CreatePost from "./Components/CreatePost/CreatePost.js";
 import SelectPhoto from "./Components/SelectPhoto/SelectPhoto.js";
 import LogIn from "./Components/LogIn/logIn";
+import Notification_page from "./Components/notification_page/notification_page";
 
 function App() {
   return (
     <div id="screen">
-      <Feed id="page1" />
-      <NotificationPage id="page2" />
-      <CreateNew />
+      <LogIn/>
+      <Feed/>
+      <Notification_page/>
       <CreateChallenge />
-      <ProfileBar />
       <ChallengeManager />
       <CreatePost />
       <SelectPhoto />
-      <LogIn/>
     </div>
   );
 }
