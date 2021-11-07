87056c43b76c0136dad9b39b84e9e77127afc149
Alex Ko <fyk211@nyu.edu>
2021-11-06 22:17:08 -0400

    update express for feed page

--- front-end/src/Components/feed/feed.js
+++ front-end/src/Components/feed/feed.js
@@ -1,18 +1,47 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./feed.css"
 import Post from "../Post/Post.js"
 
 const Feed = () => {
+
+    const [content, setContent]  = useState([]);
+    useEffect(()=>{
+      fetch('http://localhost:3001/api/feed')
+        .then(res => res.json())
+        .then(data => setContent(data));
+    }, [])
+     /*
+    useEffect(()=>{
+      const getPost = async() =>{
+        const response = await fetch('http://localhost:3001/api/');
+        const postsdata = await response.json();
+        setContent(postsdata);
+      };
+      getPost();
+    }, []);
+
+    let content = [];
+    async function getPost(){
+        const response = await fetch('http://localhost:3001/api/');
+        content = await response.json();
+        return content;
+      }
+    getPost().then(content=>{
+      console.log(content);;
+    })
+    */
+   const userPosts = content.map((post)=>{
+     return <Post name={post.name} location={post.location} content={post.text}/>
+   })
     return (
       <div id="feed" style={{ backgroundImage: "url('/images/background.png')" }}>
           <div id="notification">
             <img src="/images/bell.png" />
           </div>
-          <Post name="Henry Goulding" location="Brooklyn" content="Brooklyn Halloween Challenge: every sunday morning we will be doing a 5k vapire run from Brooklyn to Manhattan!"/>
-          <Post name="James Bond" location="Manhattan" content="Update: 2 weeks into the Kyrie Irving Leg Day Challenge. Feeling stronger!!💪"/>
-          <Post name="Ariana Grande" location="California" content="Doing my morning yoga session created by me and coach Lin. J2oin us for a spiritual and healthy start of your day!🤍🔮🐈🌫"/>
+          {userPosts}
       </div>   
     );
+  
   }
   
   export default Feed; 
