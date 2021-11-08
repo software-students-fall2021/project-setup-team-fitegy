d0cfaed0d1887cee2f6779ee7e53c239cecb26bf
Salma-Hashem <sh5640@nyu.edu>
2021-11-07 22:58:58 -0500

    app.js update

--- back-end/app.js
+++ back-end/app.js
@@ -35,6 +35,7 @@ app.get('/', (req, res) => {
 
 app.use("/api/feed", require("./routes/feed.js"))
 app.use("/api/notifications", require("./routes/notification.js"))
+app.use("/api/createChallenge", require("./routes/createChallenge.js"))
 
 // route for HTTP GET requests to /html-example
 app.get('/html-example', (req, res) => {
