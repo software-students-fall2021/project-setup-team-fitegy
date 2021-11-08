6df2db400796f593fb6483edc1f0aa237fd0a2b7
Salma-Hashem <sh5640@nyu.edu>
2021-11-07 20:39:23 -0500

    Route for Create New Page

--- front-end/src/Components/CreateNew/createNew.js
+++ front-end/src/Components/CreateNew/createNew.js
@@ -2,15 +2,16 @@ import React, { useState } from 'react';
 import "./createNew.css"
 import IconButton from '@mui/material/IconButton';
 import { AddOutlined, CreateOutlined } from '@mui/icons-material';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 
 const CreateNew = () =>{
    return(
     <div id="createNew" style={{ backgroundImage: "url('/images/background.png')" }}>
         <h1>Create a new...</h1>
-        <button id= "cancel"> Cancel</button>
+        <button id= "cancel" > Cancel</button>
             <div>
-                <IconButton aria-label= "Create a Challenge" color="primary" style={{ width: 75, height: 75 }}>
+                <IconButton component={Link} to="/create-challenge" aria-label= "Create a Challenge" color="primary" style={{ width: 75, height: 75 }}>
                     <CreateOutlined />
                 </IconButton>
                 <h2>Challenge</h2>
@@ -18,7 +19,7 @@ const CreateNew = () =>{
             </div>
             
             <div>
-                <IconButton id= "button" aria-label= "Create a Post" color= "primary" style={{ width: 75, height: 75 }} >
+                <IconButton id= "button" component={Link} to="/create-post" aria-label= "Create a Post" color= "primary" style={{ width: 75, height: 75 }} >
                     <AddOutlined />
                 </IconButton>
                 <h2>Post</h2>
