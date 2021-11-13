a0cfe77fb30c199be9b4b222b564894c428562c1
Salma-Hashem <sh5640@nyu.edu>
2021-11-12 21:27:55 -0500

    Changes to Create Challenge Testing via chai

--- back-end/test/createChallengeTest.js
+++ back-end/test/createChallengeTest.js
@@ -4,12 +4,13 @@ const chaiHttp = require('chai-http');
 chai.should()
 chai.use(chaiHttp);
 
-describe('/GET Feed Data', () => {
-    it('should return an array of objects', (done) => {
+describe('/GET Create Challenge Data', () => {
+    it('should return a confirmation route has been called', (done) => {
       chai.request(server)
           .post('/api/createChallenge')
           .end((err, res) => {
                 res.should.have.status(200);
+                
                 done();
           });
     });
