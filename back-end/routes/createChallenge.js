d5cab0abba6a5ce4f97539c68cdf6eb265e3d3b1
Corey Dubin <dubersincorporated@gmail.com>
2021-11-21 13:07:13 -0500

    some mongoose code I accidentally wrote for me

--- back-end/routes/createChallenge.js
+++ back-end/routes/createChallenge.js
@@ -2,10 +2,39 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const router = express.Router();
 const app = express();
+const mongoose = require('mongoose');
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
+/* // Define Schema for each challenge
+const ChallengeSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  dateStart: String,
+  dateEnd: String,
+});
+
+// Model for each post
+const Challenge = mongoose.model("Challenge", ChallengeSchema);
+
+// function for saving the data to MongoDB
+const SaveChallengeData = async (content) =>{
+  // instance of post model
+  const challenge1  = new Challenge(data);
+
+  // save this post to database
+  challenge1.save((error) =>{
+      if(error){
+          console.log("Oops something went wrong!")
+      }
+      else{
+          console.log("Data saved to MongoDB!")
+      }
+  })
+} */
 
 router.post("/", (req, res) => {
     const name = req.body.name
