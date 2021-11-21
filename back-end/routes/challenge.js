48ef80f7bbce70ceb98ec5a20b16100c6d768baa
Corey Dubin <dubersincorporated@gmail.com>
2021-11-21 13:06:07 -0500

    pull data from mongodb

--- back-end/routes/challenge.js
+++ back-end/routes/challenge.js
@@ -2,7 +2,15 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-let mockData;
+
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const MONGODB_URL = "mongodb+srv://fitegy:fitegy2021@fitegy.w1f4m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+mongoose.connect(MONGODB_URL);
+
+const Challenge = mongoose.model("Challenge", ChallengeSchema);
+
+/* let mockData;
 const getMockData = async () => {
     await axios
         .get("https://my.api.mockaroo.com/myapi.json?key=aea8ef40")
@@ -13,39 +21,19 @@ const getMockData = async () => {
         return {"title": data.title, "description": data.description, "dateStart": data.dateStart, "dateEnd": data.dateEnd}
     })
     return newData
-}
+} */
 
 router.get("/", async (req, res) => {
-  let newData = await getMockData();
-  console.log(newData)
-  res.json([
-    {
-      title: newData[0].title,
-      description: newData[0].description,
-      dateStart: newData[0].dateStart,
-      dateEnd: newData[0].dateEnd,
-      mainIcon: "ShieldIcon",
-      subIcon: ["DirectionsBikeIcon", "EmojiEventsIcon"],
-    },
-
-    {
-      title: newData[1].title,
-      description: newData[1].description,
-      dateStart: newData[1].dateStart,
-      dateEnd: newData[1].dateEnd,
-      mainIcon: "ShieldIcon",
-      subIcon: ["DirectionsBikeIcon", "EmojiEventsIcon"],
-    },
-
-    {
-      title: newData[2].title,
-      description: newData[2].description,
-      dateStart: newData[2].dateStart,
-      dateEnd: newData[2].dateEnd,
-      mainIcon: "ShieldIcon",
-      subIcon: ["DirectionsBikeIcon", "EmojiEventsIcon"],
-    },
-  ]);
+  const challenges = await Challenge.find();
+    const allChallenges = challenges.map((data)=>{
+        return {
+          "title": data.title, "description": data.description, "dateStart": data.dateStart,
+         "dateEnd": data.dateEnd, mainIcon: "ShieldIcon",
+         subIcon: ["DirectionsBikeIcon", "EmojiEventsIcon"],
+        }
+      })
+    console.log(allChallenges);
+    res.json(allChallenges);
 });
 
 module.exports = router;
