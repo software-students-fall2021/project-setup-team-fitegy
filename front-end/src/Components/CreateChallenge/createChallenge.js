6fa36a139afe2f8b7983c74daeb6c94b777ac653
Salma-Hashem <sh5640@nyu.edu>
2021-11-22 00:01:26 -0500

    fixing errors on create challenge route

--- front-end/src/Components/CreateChallenge/createChallenge.js
+++ front-end/src/Components/CreateChallenge/createChallenge.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./createChallenge.css"
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Link } from "react-router-dom";
 
 
 const CreateChallenge = () =>{
