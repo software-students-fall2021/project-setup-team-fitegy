6e8dfec53fea314d2e6950aaeab6eb786425d763
AnonaYY <70510392+AnonaYY@users.noreply.github.com>
2021-11-30 23:50:47 -0500

    Update createPost.js

--- back-end/routes/createPost.js
+++ back-end/routes/createPost.js
@@ -1,90 +1,147 @@
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const router = express.Router();
+// const app = express();
+// const axios = require("axios");
+
+// //Here we are configuring express to use body-parser as middle-ware.
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// // mockaroo
+// let mockData;
+// const getMockData = async () => {
+//     await axios
+//         .get("https://my.api.mockaroo.com/post_data.json?key=44aeded0")
+//         .then(apiResponse => mockData = apiResponse.data) 
+//         //.catch(err => next(err)) // pass any errors to express
+
+//     let newData = mockData.map((post) => {
+//         return {"name": post.first_name + " " + post.last_name, "location" : post.location}
+//     })
+//     return newData
+// }
+
+
+// // mongoDB 
+// require('dotenv').config({path:'../.env'});
+// const mongoose = require("mongoose");
+// const { Schema } = mongoose;
+// const MONGODB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fitegy.w1f4m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+// mongoose.connect(MONGODB_URL);
+
+// // check if mongoose is connected to MongoDB
+// /*
+// mongoose.connection.on("connected", ()=>{
+//   console.log("Mongoose is connected!");
+// })
+// */
+
+// // Define Schema for each post
+// const PostSchema = new Schema({
+//   Name: String,
+//   Location: String,
+//   postText: String, 
+//   Likes: Number
+// });
+
+// // Model for each post
+// const Post = mongoose.model("Post", PostSchema);
+
+
+// // function for saving the data to MongoDB
+// const SavePostData = async (content) =>{
+
+//     // create data
+//     let newData = await getMockData();
+//     const data= {
+//         Name: newData[0].name,
+//         Location: newData[0].location,
+//         postText: content, 
+//         Likes: 0
+//     }
+
+//     // instance of post model
+//     const Post1  = new Post(data);
+
+//     // save this post to database
+//     Post1.save((error) =>{
+//         if(error){
+//             console.log("Oops something went wrong!")
+//         }
+//         else{
+//             console.log("Data saved to MongoDB!")
+//         }
+//     })
+// }
+
+
+// // express code
+// router.post("/", async (req, res) => {
+//     //const title = req.body.title
+//     const content = req.body.content;
+//     const isPrivate = req.body.isPrivate;
+//     SavePostData(req.body.content);
+//     console.log(req.body);
+//     res.send({text: "User Input Received"});
+//   })
+
+
+// module.exports = router;
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = express.Router();
 const app = express();
-const axios = require("axios");
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// mockaroo
-let mockData;
-const getMockData = async () => {
-    await axios
-        .get("https://my.api.mockaroo.com/post_data.json?key=44aeded0")
-        .then(apiResponse => mockData = apiResponse.data) 
-        //.catch(err => next(err)) // pass any errors to express
+//mongoDB code 
 
-    let newData = mockData.map((post) => {
-        return {"name": post.first_name + " " + post.last_name, "location" : post.location}
-    })
-    return newData
-}
-
-
-// mongoDB 
-require('dotenv').config({path:'../.env'});
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
-const MONGODB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fitegy.w1f4m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+require('dotenv').config({path:'../.env'})
+const {Schema}= mongoose;
+const MONGODB_URL = 'mongodb+srv://${provess.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fitegy.w1f4m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose.connect(MONGODB_URL);
 
-// check if mongoose is connected to MongoDB
-/*
-mongoose.connection.on("connected", ()=>{
-  console.log("Mongoose is connected!");
-})
-*/
-
 // Define Schema for each post
-const PostSchema = new Schema({
-  Name: String,
-  Location: String,
-  postText: String, 
-  Likes: Number
+const PostSchema = new mongoose.Schema({
+  title: String,
+  content: String,
 });
 
 // Model for each post
 const Post = mongoose.model("Post", PostSchema);
 
-
 // function for saving the data to MongoDB
 const SavePostData = async (content) =>{
+  // instance of post model
+  const post1  = new Post(data);
 
-    // create data
-    let newData = await getMockData();
-    const data= {
-        Name: newData[0].name,
-        Location: newData[0].location,
-        postText: content, 
-        Likes: 0
-    }
+  // save this post to database
+  post1.save((error) =>{
+      if(error){
+          console.log("Oops something went wrong!")
+      }
+      else{
+          console.log("Data saved to MongoDB!")
+      }
+  })
+} 
 
-    // instance of post model
-    const Post1  = new Post(data);
-
-    // save this post to database
-    Post1.save((error) =>{
-        if(error){
-            console.log("Oops something went wrong!")
-        }
-        else{
-            console.log("Data saved to MongoDB!")
-        }
-    })
-}
-
-
-// express code
-router.post("/", async (req, res) => {
-    //const title = req.body.title
-    const content = req.body.content;
-    const isPrivate = req.body.isPrivate;
-    SavePostData(req.body.content);
+router.post("/", (req, res) => {
+    const title = req.body.title
+    const content = req.body.content
+    // const dates = req.body.date
+    const isPrivate = req.body.isPrivate
+    // now do something amazing with this data...
+    // ... then send a response of some kind
     console.log(req.body);
     res.send({text: "User Input Received"});
   })
 
 
 module.exports = router;
+
