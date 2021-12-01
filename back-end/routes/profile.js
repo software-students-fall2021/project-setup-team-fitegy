9ff4213274ba0aca3ee9ca2e3c7ec30e4e69a142
inayoun <inayoun0519@gmail.com>
2021-11-30 21:11:22 -0500

    Updating database

--- back-end/routes/profile.js
+++ back-end/routes/profile.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
+// Mock data
 let mockData;
 const getMockData = async () => {
     await axios
@@ -12,8 +13,17 @@ const getMockData = async () => {
     return mockData
 }
 
+// MongoDB
+const mongoose = require("mongoose");
+require('dotenv').config({path:'../.env'})
+const { Schema } = mongoose;
+const MONGODB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fitegy.w1f4m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+mongoose.connect(MONGODB_URL);
+//TODO: require function
+//TODO: create variable
+
 router.get('/', async (req, res) => {
-  let newData = await getMockData();
+  let newData = await getMockData(); //TODO: change to variable.find()
   res.json([
       {ongoing: newData.ongoing_challenges, done: newData.done_challenges, points: newData.points},
   ]);
