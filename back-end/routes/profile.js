b0d9f72225699471cc2bc2fb9d6f5c1cc31dfaf3
inayoun <inayoun0519@gmail.com>
2021-12-02 22:24:30 -0500

    Update profile.js

--- back-end/routes/profile.js
+++ back-end/routes/profile.js
@@ -1,31 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+require("./")
 
-// Mock data
-let mockData;
-const getMockData = async () => {
-    await axios
-        .get("https://my.api.mockaroo.com/statistics.json?key=08be32e0")
-        .then(apiResponse => mockData = apiResponse.data) 
-        .then(() => console.log(mockData))
+// let mockData;
+// const getMockData = async () => {
+//     await axios
+//         .get("https://my.api.mockaroo.com/statistics.json?key=08be32e0")
+//         .then(apiResponse => mockData = apiResponse.data) 
+//         .then(() => console.log(mockData))
 
-    return mockData
-}
+//     return mockData
+// }
 
 // MongoDB
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 require('dotenv').config({path:'../.env'})
 const { Schema } = mongoose;
 const MONGODB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fitegy.w1f4m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 mongoose.connect(MONGODB_URL);
-//TODO: require function
-//TODO: create variable
+const Stats = mongoose.model("Stats")
 
 router.get('/', async (req, res) => {
-  let newData = await getMockData(); //TODO: change to variable.find()
+  let newData = await Stats.find(); //TODO: change to variable.find()
   res.json([
-      {ongoing: newData.ongoing_challenges, done: newData.done_challenges, points: newData.points},
+      {ongoing: newData.ongoing_challenges, done: newData.done_challenges},
   ]);
 })
 
