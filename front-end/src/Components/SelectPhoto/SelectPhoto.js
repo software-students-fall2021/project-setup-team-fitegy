b29e9833525448c39eb6edc27f998b6f2ce6e03a
AnonaYY <70510392+AnonaYY@users.noreply.github.com>
2021-12-10 18:23:59 -0500

    Update SelectPhoto.js

--- front-end/src/Components/SelectPhoto/SelectPhoto.js
+++ front-end/src/Components/SelectPhoto/SelectPhoto.js
@@ -2,34 +2,6 @@ import React, { useState, useEffect } from 'react'
 import './SelectPhoto.css'
 import ImageUploader from "react-images-upload";
 const multer = require("multer");
-// const SelectPhoto= (props) => {
-//   const [pictures, setPictures] = useState([]);
-
-  // const onDrop = picture => {
-  //   setPictures([...pictures, picture]);
-  // };
-  //   return (
-  //     <div className="SelectPhoto" style={{ backgroundImage: "url('/images/background.png')" }}>
-  //         {/* <h1>All Photos</h1>
-  //       <button onClick={selectPhoto}>Select</button> */}
-  //   <ImageUploader 
-  //     {...props}
-  //     withIcon={true}
-  //     onChange={onDrop}
-  //     imgExtension={[".jpg", ".gif", ".png", ".gif"]}
-  //     maxFileSize={5242880}
-  //     />
-  // </div>
-  //   )
-  // }
-
-  // const SelectPhoto =(props) => {
-
-  // }
-
-
-
-
 
 const SelectPhoto = (props) => {
     // const [title, setName] = React.useState("");
