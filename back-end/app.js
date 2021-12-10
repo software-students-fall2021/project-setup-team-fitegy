45eaf767b5e43d1418c2313e237e309106ffaa84
AnonaYY <70510392+AnonaYY@users.noreply.github.com>
2021-12-10 15:53:03 -0500

    Update app.js

--- back-end/app.js
+++ back-end/app.js
@@ -23,8 +23,8 @@ app.use(cors());
 app.use(morgan("dev")); // morgan has a few logging default styles - dev is a nice concise color-coded style
 
 // use the bodyparser middleware to parse any data included in a request
-app.use(bodyParser.json()); // decode JSON-formatted incoming POST data
-app.use(bodyParser.urlencoded({ extended: true })); // decode url-encoded incoming POST data
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 // make 'public' directory publicly readable with static content
 app.use("/static", express.static("public"));
