c19ccba427d7c0eca35cbe71633b70f2b013af6f
Christian Kovacs <66984388+ckovacs1@users.noreply.github.com>
2021-12-11 16:47:50 -0500

    Added Password Form

--- front-end/src/Components/Settings/BackButton.js
+++ front-end/src/Components/Settings/BackButton.js
@@ -6,7 +6,8 @@ function BackButton() {
 
         <div className='back'>
             {/* Link to go back, change to Profile Page, and change link to button */}
-            <a href="/profile"> Back </a>
+            
+            <a href="/profile" > Back </a>
         </div>
 
     );
