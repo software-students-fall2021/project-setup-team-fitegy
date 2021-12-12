234cb9e49d534400ebf3e16da8bd17c00ed138b7
Corey Dubin <dubersincorporated@gmail.com>
2021-12-12 15:58:12 -0500

    fix createPostTest

--- back-end/test/createPostTest.js
+++ back-end/test/createPostTest.js
@@ -1,16 +1,17 @@
-const server = require('../app');
+const server = require("../app");
 const chai = require("chai");
-const chaiHttp = require('chai-http');
-chai.should()
+const chaiHttp = require("chai-http");
+chai.should();
 chai.use(chaiHttp);
 
-describe('/GET Post Data', () => {
-    it('should return an array of objects', (done) => {
-      chai.request(server)
-          .post('/api/createPost')
-          .end((err, res) => {
-                res.should.have.status(200);
-                done();
-          });
-    });
+describe("/GET Post Data", () => {
+  it("should return an array of objects", (done) => {
+    chai
+      .request(server)
+      .options("/api/createPost")
+      .end((err, res) => {
+        res.should.have.status(204);
+        done();
+      });
+  });
 });
